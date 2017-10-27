381b82c1b0fb24fd32a2b0186dd56c9970900441
Philipp Bornstedt <philipp.bornstedt@innogames.com>
2017-10-27 12:24:05 +0200

    Added canvas and cell labels

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour
 {
@@ -11,10 +12,15 @@ public class HexGrid : MonoBehaviour
     [SerializeField]
     private HexCell _cellPrefab;
 
+    [SerializeField]
+    private Text _cellLabel;
+
+    private Canvas _canvas;
     private HexCell[] _cells;
 
     private void Awake()
     {
+        _canvas = GetComponentInChildren<Canvas>();
         _cells = new HexCell[_height * _width];
 
         for (int z = 0, i = 0; z < _height; z++)
@@ -34,7 +40,19 @@ public class HexGrid : MonoBehaviour
         position.z = z * 10f;
 
         HexCell cell = _cells[index] = Instantiate(_cellPrefab);
+        cell.name = "Cell " + index;
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
+
+        CreateLabel(position.x, position.z, index);
+    }
+
+    private void CreateLabel(float posX, float posZ, int index)
+    {
+        Text label = Instantiate(_cellLabel);
+        label.name = "Cell-Label " + index;
+        label.rectTransform.SetParent(_canvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(posX, posZ);
+        label.text = posX + "\n" + posZ;
     }
 }
